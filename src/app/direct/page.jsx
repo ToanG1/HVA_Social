31b4a785cf797a840c76630f45ecaa747cc9924b
ToanG1 <toandinh01675@gmail.com>
2024-01-27 23:21:38 +0700

    create: direct page

--- src/app/direct/page.jsx
+++ src/app/direct/page.jsx
@@ -1,13 +1,103 @@
 import styles from './page.module.scss';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import SideNav from '@/components/SideNav/sideNav';
+import defaultImg from '@/assets/images/mouse.png';
 
-// import SideNav from '@/components/SideNav';
-
+const directData = [
+  {
+    username: 'ngot_303',
+    avatar: defaultImg,
+    lastMessage: {
+      type: 'text',
+      content: 'Hello',
+      createdAt: '2024-01-21T13:55:23.366+00:00',
+    },
+  },
+  {
+    username: 'ngot_303',
+    avatar: defaultImg,
+    lastMessage: {
+      type: 'text',
+      content: 'Hello',
+      time: '2024-01-21T13:55:23.366+00:00',
+    },
+  },
+  {
+    username: 'ngot_303',
+    avatar: defaultImg,
+    lastMessage: {
+      type: 'text',
+      content: 'Hello',
+      createdAt: '2024-01-21T13:55:23.366+00:00',
+    },
+  },
+  {
+    username: 'ngot_303',
+    avatar: defaultImg,
+    lastMessage: {
+      type: 'text',
+      content: 'Hello',
+      createdAt: '2024-01-21T13:55:23.366+00:00',
+    },
+  },
+];
 
 export default function Direct() {
   return (
     <>
-      {/* <SideNav /> */}
-      <h1 className={styles.directContainer}>Hello direct</h1>
+      <SideNav />
+      <div className={styles.directContainer}>
+        <div className={styles.directSideBar}>
+          <div className={styles.directHeader}>
+            <h2 className={styles.username}>ngot_303</h2>
+            <i>
+              <FontAwesomeIcon
+                icon={faPenToSquare}
+                style={{ fontSize: '20px' }}
+              />
+            </i>
+          </div>
+          <div className={styles.directList}>
+            <div className={styles.directTitle}>
+              <h4>Messages</h4>
+              <h4>Requests</h4>
+            </div>
+
+            {directData.map((item, i) => {
+              return <DirectItem userInfo={item} key={i} />;
+            })}
+          </div>
+        </div>
+        <div className={styles.directBox}></div>
+      </div>
+    </>
+  );
+}
+
+function DirectItem({ userInfo }) {
+  dayjs.extend(relativeTime);
+  return (
+    <>
+      <div className={styles.directItem}>
+        <Image src={userInfo.avatar || defaultImg} alt="user avatar" />
+        <div>
+          <h4>{userInfo.username || 'username'}</h4>
+          <div>
+            <p>
+              {userInfo.lastMessage.type === 'text'
+                ? userInfo.lastMessage.content
+                : 'image'}
+            </p>
+            <FontAwesomeIcon icon={faCircle} style={{ fontSize: '3px' }} />
+            <p>{dayjs(userInfo.lastMessage.createdAt).fromNow(true)}</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,17 @@
 import styles from './page.module.scss';
 
-import SideNav from '@/components/sideNav/sideNav';
+import SideNav from '@/components/SideNav/SideNav';
 import { PostItem } from '@/components/PostItem';
 
 export default function Home() {
   return (
     <>
       <SideNav />
-      <div className={styles.homeContainer}>{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-        <PostItem key={i} /> 
-      ))}</div>
+      <div className={styles.homeContainer}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+          <PostItem key={i} />
+        ))}
+      </div>
     </>
   );
 }
